b2cfb19b491890909af7be298b847f14653a27b1
nmuuln <111161236+nmuuln@users.noreply.github.com>
2025-02-07 13:56:29 +0800

    change the css of duplicated header css and fix the bug in header footer load js

--- js/load-header-footer.js
+++ js/load-header-footer.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     loadHTML("header.html", "header", updateActiveNavButton);
     loadHTML("footer.html", "footer");
-    loadPageContent(window.location.pathname);
 
     document.querySelectorAll("nav .nav-buttons a, div.logo a").forEach(link => {
         link.addEventListener("click", function(event) {
@@ -27,42 +26,6 @@ function loadHTML(url, elementId, callback) {
             if (callback) callback();
         })
         .catch(error => console.error('Error loading HTML:', error));
-}
-
-function loadPageContent(page) {
-    const contentElement = document.querySelector("main");
-
-    fetch(page)
-        .then(response => {
-            if (!response.ok) throw new Error("Page not found");
-            return response.text();
-        })
-        .then(html => {
-            const tempDiv = document.createElement("div");
-            tempDiv.innerHTML = html;
-
-            const newContent = tempDiv.querySelector("main").innerHTML;
-            contentElement.innerHTML = newContent;
-
-            const newTitle = tempDiv.querySelector("title");
-            if (newTitle) {
-                document.title = newTitle.innerText;
-            }
-
-            updateScripts(tempDiv);
-            updateActiveNavButton();
-        })
-        .catch(error => console.error("Error loading page content:", error));
-}
-
-function updateScripts(tempDiv) {
-    tempDiv.querySelectorAll("script").forEach(oldScript => {
-        const newScript = document.createElement("script");
-        newScript.src = oldScript.src;
-        newScript.textContent = oldScript.textContent;
-        newScript.async = oldScript.async;
-        document.body.appendChild(newScript);
-    });
 }
 
 function updateActiveNavButton() {
